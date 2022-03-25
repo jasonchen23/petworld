b75699c3f66fbc17d711a53f150bb1a847ca581d
Jasonchen23 <kevin3121961@gmail.com>
2022-03-25 10:22:43 +0800

    add create

--- member/create_user.php
+++ member/create_user.php
@@ -7,15 +7,25 @@ $phone=$_POST["phone"];
 $address=$_POST["address"];
 $job=$_POST["job"];
 $age=$_POST["age"];
-$sql="INSERT INTO user (account,password,email,phone,address,job,age,valid)
-VALUES ('$account','$password','$email','$phone','$address','$job','$age',1)";
-if ($conn->query($sql) === TRUE) {
-    echo "Signup Success";
-    $last_id = $conn -> insert_id;
-    echo "id : $last_id" ;
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-};
+$sql="SELECT * FROM user WHERE email='$email'";
+// echo $sql;
+if($conn -> query($sql) == TRUE) {
+    $result = $conn->query($sql);
+    $userCount=$result->num_rows;
+
+    if($userCount>0) {
+        echo "email has been registered";
+    }else{
+        $sql="INSERT INTO user (account,password,email,phone,address,job,age,valid)
+        VALUES ('$account','$password','$email','$phone','$address','$job','$age',1)";
+        if ($conn->query($sql) === TRUE) {
+            echo "Signup Success";
+            $last_id = $conn -> insert_id;
+            echo "id : $last_id" ;
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        };
+    }}
 $conn->close();
 // $result=$conn->query($sql);
 // $data=$result->fetch_all();
